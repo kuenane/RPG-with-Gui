8b70f6af7a816ef7e6fa2347206712ff7f8c4352
Lebohang KUENANE <lkuenane@e4r12p11.wethinkcode.co.za>
2018-07-17 17:11:30 +0200

    chess

---
+++ Launcher.java/src/za/Wethinkcode/tilegame/Launcher.java
@@ -0,0 +1,10 @@
+package za.Wethinkcode.tilegame;
+
+import za.Wethinkcode.tilegame.display.Display;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        new Display("Title!", 700, 900);
+    }
+}
